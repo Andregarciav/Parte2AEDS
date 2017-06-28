cb4390e9f3ee7134547c7e22958315f051f28dc0
Andregarciav <andregarcia@ufmg.br>
2017-06-28 14:50:35 -0300

    Update ap01ex1.c

--- ap01ex1.c
+++ ap01ex1.c
@@ -1,33 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 10
+typedef struct pessoa
+  {
+    char nome[20];
+    int idade, peso;
+    float altura;
+  }INDIVIDUO;
+INDIVIDUO* cadastro( int N )
+{
+  INDIVIDUO* cadastro = (INDIVIDUO*)malloc(sizeof(INDIVIDUO)*MAX);
 
-int *alocar(int N){
-  int *v, i;
-  v = (int*)malloc(N*sizeof(int)); //aloca dinamicamente memória sem inicializar
-  /*calloc(N, sizeof(int)) //aloca dinamicamente memória inicializando, maior custo*/
-  for (i=0; i<N;i++){
-    v[i]=i;
+  int i;
+  for ( i = 0 ; i < N ; i++)
+  {
+    printf("Digite o nome do paciente: ");
+    scanf("%s" , cadastro[i].nome );
+    printf("Digite a idade do paciente: ");
+    scanf("%d" , &cadastro[i].idade );
+    printf("Digite o peso do paciente: ");
+    scanf("%d" , &cadastro[i].peso );
+    printf("Digite o altura em metros do paciente: ");
+    scanf("%f%*c" , &cadastro[i].altura );
   }
-  return v;
+  return cadastro;
 }
-
-
-int main() {
-  int N, i;
-  int *v;
-  scanf(" %d", &N);
-  v = alocar(N);
-  for ( i=0; i<N ; i++ ){
-    printf("%d ", v[i]);
-  }printf("\n");
-  free(v);
-  scanf(" %d", &N);
-  v = alocar(N);
-  for ( i=0; i<N ; i++ ){
-    printf("%d ", v[i]);
-  }printf("\n");
-  free(v);
-
+void imprime(int N , INDIVIDUO* imprime)
+{
+  int i;
+  printf("Paciente\nNome\tidade\tpeso\taltura\n");
+  for ( i = 0 ; i < N ; i++)
+  {
+    printf("%s\t" , imprime[i].nome );
+    printf("%d\t" , imprime[i].idade );
+    printf("%d\t" , imprime[i].peso );
+    printf("%.2f\n" , imprime[i].altura );
+  }
+}
+int main()
+{
+  INDIVIDUO *V;
+  int Num;
+    printf("Cadastro de Pacientes\nDigite o numero de pacientes(1 a 10): ");
+  scanf("%d%*c", &Num);
+  while ( Num > MAX )
+  {
+  printf("Voce digitou um numero superior ao permitido\n");
+  printf("Digite o numero de pacientes(1 a 10): ");
+  scanf("%d", &Num);
+  }
+  V = cadastro (Num);
+  imprime ( Num , V);
+  free(V);
   return 0;
-
 }
